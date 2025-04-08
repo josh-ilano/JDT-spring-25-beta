0dfd72fd9c8ee78832c21c98b5536b73abe81e95
Josh Ilano <jilano1@bu.edu>
2025-04-08 19:21:14 -0400

    changes

--- workspace/frontend/src/App.jsx
+++ workspace/frontend/src/App.jsx
@@ -1,6 +1,33 @@
+import Navbar from "./components/navbar/navbar"
+import Profile from "./components/profile/profile"
+import Projects from "./components/projects/projects"
+import Experiences from "./components/experiences/experiences"
+import About from './components/about/about'
+import Chat from './components/chat/chat'
+import './App.css'
+
 function App() {
     return (
-        <h1>Hello World</h1>
+        <>
+            <Navbar/>
+            <Profile/>
+            <About/>
+            <Chat/>
+            <div id="projects">
+                <h2>Projects</h2>
+                <Projects name="Project 1 Name" description="Project description here" github="https://github.com/josh-ilano/OrientationProduct"/>
+                <Projects name="Project 2 Name" description="Project description here" github="https://github.com/josh-ilano/OrientationProduct"/>
+            </div> 
+            <div id="exp">
+                <h2>Experiences</h2>
+                    <Experiences 
+                        title="Job Title" 
+                        info="Company Name, Location, States" 
+                        dates="Dates - Dates" 
+                        bullets={["Point 1", "Point 2", "Point 3"]}>
+                    </Experiences>
+            </div>
+        </>
     )
 }
 
---
+++ workspace/frontend/src/components/about/about.jsx
@@ -0,0 +1,45 @@
+import './about.css'
+import CSS from '../../assets/css.svg'
+import HTML from '../../assets/html.svg'
+import JS from '../../assets/js.svg'
+import PY from '../../assets/py.svg'
+import { useState } from 'react'
+
+function About () {
+    const [display, setDisplay] = useState('block')
+    const [buttonText, setButtonText] = useState('Hide')
+
+    function toggle() {
+        if(display == 'none') {
+            setDisplay('block')
+            setButtonText('Hide')
+        } else {
+            setDisplay('none')
+            setButtonText('Show')
+        }
+    }
+
+    return (
+        <div id="about">
+            <h2>
+                <button className="toggler" onClick={toggle}>{buttonText}</button>
+                About me
+            </h2>
+            <div style={{display: display}}>
+                <p>
+                    "I'm a student at Boston University studying Computer Science. I am currently a 
+                    member of the the Hack4Impact organization. "
+                </p>
+            </div>
+            <h3>My Skills</h3>
+            <div className="Icons">
+                <img width="60" src={HTML} />
+                <img width="60" src={CSS}  />
+                <img width="60" src={PY}   />
+                <img width="60" src={JS}   />
+            </div>
+        </div>
+    )
+}
+
+export default About
